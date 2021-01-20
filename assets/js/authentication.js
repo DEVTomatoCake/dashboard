b8b18687d1b732756d878956f3745efb1f81b424
booky10 <github@booky.tk>
2021-01-20 14:20:58 +0100

    added some debugging

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -1,17 +1,20 @@
 function authRedirect() {
   const params = new URLSearchParams(window.location.search);
+  let href;
 
   if (!params.has('code') && !getCookie('access_token')) {
-    location.href = 'https://' + location.host + '/login';
+    href = 'https://' + location.host + '/login';
   } else {
     setCookie('access_token', params.get('code'), 7);
 
     if (params.has('guild_id')) {
-      location.href = 'https://' + location.host + '/dashboard?guild=' + params.get('guild_id');
+      href = 'https://' + location.host + '/dashboard?guild=' + params.get('guild_id');
     } else {
-      location.href = 'https://' + location.host + '/dashboard';
+      href = 'https://' + location.host + '/dashboard';
     }
   }
+
+  console.log('Would be redirecting to ' + href + '!');
 }
 
 function redirectIfAuth() {

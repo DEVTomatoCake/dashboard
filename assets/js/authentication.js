8d563ae6f9990d8f08d5ace8d1795587b03d6183
booky10 <github@booky.tk>
2021-01-20 14:18:25 +0100

    added login functionality

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -1,9 +1,20 @@
 function authRedirect() {
-  if (getCookie('access_token')) return;
-  location.href = 'https://' + location.host + '/invite';
+  const params = new URLSearchParams(window.location.search);
+
+  if (!params.has('code') && !getCookie('access_token')) {
+    location.href = 'https://' + location.host + '/login';
+  } else {
+    setCookie('access_token', params.get('code'), 7);
+
+    if (params.has('guild_id')) {
+      location.href = 'https://' + location.host + '/dashboard?guild=' + params.get('guild_id');
+    } else {
+      location.href = 'https://' + location.host + '/dashboard';
+    }
+  }
 }
 
-function redirectAuth() {
+function redirectIfAuth() {
   if (!getCookie('access_token')) return;
   location.href = 'https://' + location.host + '/dashboard';
 }

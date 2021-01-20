181bc5baef426ada4b9cff78fb9eb6c56e7dfd6a
booky10 <github@booky.tk>
2021-01-20 17:35:40 +0100

    fixed the logic hopefully

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -1,23 +1,20 @@
 function authRedirect() {
-  const params = new URLSearchParams(window.location.search);
-  let href;
+  const params = new URLSearchParams(location.search);
 
   if (!params.has('code') && !getCookie('access_token')) {
-    href = 'https://' + location.host + '/login';
-  } else {
+    location.href = 'https://' + location.host + '/login';
+  } else if (!getCookie('access_token')) {
     setCookie('access_token', params.get('code'), 7);
 
     if (params.has('guild_id')) {
-      href = 'https://' + location.host + '/dashboard?guild=' + params.get('guild_id');
+      location.href = 'https://' + location.host + '/dashboard?guild=' + params.get('guild_id');
     } else {
-      href = 'https://' + location.host + '/dashboard';
+      location.href = 'https://' + location.host + '/dashboard';
     }
   }
-
-  console.log('Would be redirecting to ' + href + '!');
 }
 
 function redirectIfAuth() {
   if (!getCookie('access_token')) return;
-  console.log('Would be redirecting to https://' + location.host + '/dashboard!');
+  location.href = 'https://' + location.host + '/dashboard';
 }

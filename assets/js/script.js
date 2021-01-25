7751280ac925d7468ca9b0137a3d1caa129ac1ae
booky10 <github@booky.tk>
2021-01-25 15:36:49 +0100

    set opacity if not set

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,14 @@ function toggleMenu() {
 }
 
 function fadeOut(element) {
+  if(!element.style.opacity) element.style.opacity = 1;
   element.style.opacity -= 0.05;
   if (element.style.opacity >= 0) setTimeout(() => fadeOut(element), 25);
   else element.remove();
 }
 
 function fadeIn(element) {
+  if(!element.style.opacity) element.style.opacity = 0;
   element.style.opacity = parseFloat(element.style.opacity) + 0.05;
   if (element.style.opacity < 1) setTimeout(() => fadeIn(element), 25);
 }

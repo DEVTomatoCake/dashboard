3c6fe003efeac1a86a78288701800632d1a97dd1
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2021-02-16 10:56:32 +0100

    Update transformer.js

--- assets/js/api/transformer.js
+++ assets/js/api/transformer.js
@@ -125,15 +125,15 @@ function getSettingsHTML(guild) {
                 '<option value="false" selected>false</option>' +
                 '</select>'
                 '<br /><br />';
-            } else if (setting.type.startWith("select") && setting.value == "false") {
+            } else if (setting.type.startWith("select")) {
               selectable = setting.type.replace("select,", "").split(" ")
               console.log(selectable)
-              text += '' +
+              /*text += '' +
                 '<p>' + setting.help + '</p>' +
                 '<select class="setting" id="' + setting.key + '" name="' + setting.key + '">' +
                 '<option value="true">-</option>' +
                 '</select>'
-                '<br /><br />';
+                '<br /><br />';*/
             } else {
               text += '' +
                 '<p>' + setting.help + '</p>' +

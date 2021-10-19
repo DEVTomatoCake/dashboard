d582bdec008ba424f7dc80563656372d6a940742
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2021-10-19 09:44:33 +0200

    *hilfe sieht das schrecklich aus*

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,20 +23,3 @@ function toggleMenu() {
   document.querySelector('.menu-toggle').classList.toggle('active');
   document.querySelector('.navigation').classList.toggle('active');
 }
-
-function fadeOut(element) {
-  if (!element) return;
-  if (!element.style.opacity) element.style.opacity = 1;
-
-  element.style.opacity = parseFloat(element.style.opacity) - 0.05;
-  if (element.style.opacity >= 0) setTimeout(() => fadeOut(element), 25);
-  else element.remove();
-}
-
-function fadeIn(element) {
-  if (!element) return;
-  if (!element.style.opacity) element.style.opacity = 0;
-
-  element.style.opacity = parseFloat(element.style.opacity) + 0.05;
-  if (element.style.opacity < 1) setTimeout(() => fadeIn(element), 25);
-}

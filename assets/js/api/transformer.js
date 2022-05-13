2adcadf52d2cd90275360a116a9b2855d1db89de
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-05-13 09:59:27 +0200

    Add Websockets (#4)
    * gewebte socken oder so, hab ka davon
    
    * ws close + error handling
    
    * global var
    
    * Maybe reconnect and save settings
    
    * save settings
    
    * Fix send data
    
    * Better code for hiding
    
    * Save settings as object not string
    
    * fix hidden
    
    * auto run connect function
    
    * Add error handling for invalid json
    
    * Update index.html
    
    * :art: Style fixes
    
    * semikolon oder so
    
    * Add modified sockette + change ws code
    
    * update websocket url + fix msg parsing
    
    * Fix initial send
    
    * remove old ws code + change ws start
    
    WS startet jetzt nicht beim Seitenladen, sondern erst nachdem die Anmeldung usw abgeschlossen wurden und die Parameter überprüft sind
    
    * Send guild + token in request
    
    * (was mache ich hier) add load settings support
    
    * keeek promises
    
    * Update save settings
    
    * Fix saving msg + settings value
    
    * Fix saving msg²
    
    * Remove useless encoding
    
    * maybe fix sidebar
    
    * really fix saving msg + allow sharing constants
    
    * gewebte socken oder so, hab ka davon
    
    * ws close + error handling
    
    * global var
    
    * Maybe reconnect and save settings
    
    * save settings
    
    * Fix send data
    
    * Better code for hiding
    
    * Revert "Merge branch 'websockets' of https://github.com/DEVTomatoCake/dashboard into websockets"
    
    This reverts commit c243572fd4bebfea742f78bac9ac2b0fc1c63b49, reversing
    changes made to 34ae2ebfa03388a9fa702e5a98faf60e36a585ca.
    
    * css update
    
    * :twisted_rightwards_arrows: Merge branches
    
    * remove luxon, update uptime, fix setting tabs
    
    * change login query on beta²

--- assets/js/api/transformer.js
+++ assets/js/api/transformer.js
@@ -72,7 +72,7 @@ function getGuildsHTML() {
 	return new Promise(resolve => {
 		getGuilds()
 			.then(json => {
-				if (json.status === 'success') {
+				if (json.status == 'success') {
 					let text = '';
 					json.data.sort((a, b) => {
 						if (a.activated && b.activated) return 0;
@@ -106,76 +106,72 @@ function getGuildsHTML() {
 	});
 }
 
-function getSettingsHTML(guild) {
-	return new Promise(resolve => {
-		getSettings(guild)
-			.then(json => {
-				if (json.status === 'success') {
-					let text = '';
-					var categories = [];
-					var categoryData = [];
+async function getSettingsHTML(guild, json) {
+	if (!json) {
+		console.error("Missing json in getSettingsHTML");
+		return ('' +
+			'<h1>Es gab einen Fehler beim Verarbeiten der API-Abfrage!</h1>' +
+			'<h1>Guck in deine Browserkonsole, um mehr zu erfahren!</h1>');
+	};
 
-					multiselect = json.constant.multiselect
-					const integer = json.constant.integer
+	if (json.status == 'success') {
+		let text = '';
+		var categories = [];
+		var categoryData = [];
 
-					json.data.forEach(setting => {
-						temp = '';
-						if (!setting.possible) {
-							if (integer.includes(setting.key)) temp += '' +
-								'<p>' + setting.help + '</p>' +
-								'<input type="number" min="0" max="9999" class="setting" id="' + setting.key + '" name="' + setting.key + '" value="' + setting.value + '">';
-							else temp += '' +
-								'<p>' + setting.help + '</p>' +
-								'<input class="setting" size="' + (screen.width > 500 ? 38 : 20) + '" id="' + setting.key + '" name="' + setting.key + '" value="' + setting.value + '">';
-						} else {
-							const possible = setting.possible;
+		multiselect = json.constant.multiselect;
 
-							if (multiselect.includes(setting.key)) {
-								temp += '<p>' + setting.help + '</p><select multiple class="setting" id="' + setting.key + '" name="' + setting.key + '">';
-								var selected = [];
-								var i = 0;
-								Object.keys(possible).forEach(key => {
-									if (key == "") return
-									setting.value.split(",").forEach(data => {
-										if (data == key.replace('_', '')) selected.push(i);
-									});
-									i++;
-									if (key != "") temp += '<option value="' + key.replace('_', '') + '" ' + (setting.value == key.replace('_', '') ? 'selected' : '') + '>' + possible[key] + '</option>';
-								});
-								setTimeout(() => {
-									drops.push({key: setting.key, data: new drop({selector: "#" + setting.key, preselected: selected})});
-								}, 2000);
-							} else {
-								temp += '<p>' + setting.help + '</p><select class="setting" id="' + setting.key + '" name="' + setting.key + '">';
-								Object.keys(possible).forEach(key => temp += '<option value="' + key.replace('_', '') + '" ' + (setting.value == key.replace('_', '') ? 'selected' : '') + '>' + possible[key] + '</option>');
-							}
-							temp += '</select>';
-						}
-						if (setting.category && !categories.includes(setting.category)) categories.push(setting.category);
-						if (setting.category) categoryData.push([setting.category, temp + '<br><br>']);
-					});
+		json.data.forEach(setting => {
+			let temp = "";
+			if (!setting.possible) {
+				if (json.constant.integer.includes(setting.key)) temp += '' +
+					'<p>' + setting.help + '</p>' +
+					'<input type="number" min="0" max="9999" class="setting" id="' + setting.key + '" name="' + setting.key + '" value="' + setting.value + '">';
+				else temp += '' +
+					'<p>' + setting.help + '</p>' +
+					'<input class="setting" size="' + (screen.width > 500 ? 38 : 20) + '" id="' + setting.key + '" name="' + setting.key + '" value="' + setting.value + '">';
+			} else {
+				var possible = setting.possible;
+				if (typeof possible == "string") possible = json.constant[possible]
 
-					categories.forEach(category => {
-						text += '<h2 id="' + category + '">' + category.charAt(0).toUpperCase() + category.slice(1) + '</h2><br>';
-						categoryData.forEach(data => {
-							if (category == data[0]) text += data[1];
+				if (multiselect.includes(setting.key)) {
+					temp += '<p>' + setting.help + '</p><select multiple class="setting" id="' + setting.key + '" name="' + setting.key + '">';
+					var selected = [];
+					var i = 0;
+					Object.keys(possible).forEach(key => {
+						if (key == "") return
+						setting.value.split(",").forEach(data => {
+							if (data == key.replace('_', '')) selected.push(i);
 						});
-					})
-
-					resolve('<center><h1>Einstellungen von <span class="accent">' + json.name + '</span></h1></center>' + text);
+						i++;
+						if (key != "") temp += '<option value="' + key.replace('_', '') + '" ' + (setting.value == key.replace('_', '') ? 'selected' : '') + '>' + possible[key] + '</option>';
+					});
+					setTimeout(() => {
+						drops.push({key: setting.key, data: new drop({selector: "#" + setting.key, preselected: selected})});
+					}, 2000);
 				} else {
-					resolve('' +
-						'<h1>Es gab einen Fehler beim Verarbeiten der API-Abfrage!</h1>' +
-						'<h1>' + json.message + '</h1>');
+					temp += '<p>' + setting.help + '</p><select class="setting" id="' + setting.key + '" name="' + setting.key + '">';
+					Object.keys(possible).forEach(key => temp += '<option value="' + key.replace('_', '') + '" ' + (setting.value == key.replace('_', '') ? 'selected' : '') + '>' + possible[key] + '</option>');
 				}
-			})
-			.catch(error => {
-				console.error(error);
-				resolve('' +
-					'<h1>Es gab einen Fehler beim Verarbeiten der API-Abfrage!</h1>' +
-					'<h1>Guck in deine Browserkonsole, um mehr zu erfahren!</h1>');
+				temp += '</select>';
+			}
+			if (setting.category && !categories.includes(setting.category)) categories.push(setting.category);
+			if (setting.category) categoryData.push([setting.category, temp + '<br><br>']);
+		});
+
+		categories.forEach(category => {
+			text += '<h2 id="' + category + '">' + category.charAt(0).toUpperCase() + category.slice(1) + '</h2><br>';
+			categoryData.forEach(data => {
+				if (category == data[0]) text += data[1];
 			});
-	});
+		})
+
+		return ('<center><h1>Einstellungen von <span class="accent">' + json.name + '</span></h1></center>' + text);
+	} else {
+		return ('' +
+			'<h1>Es gab einen Fehler beim Verarbeiten der API-Abfrage!</h1>' +
+			'<h1>' + json.message + '</h1>');
+	};
 }
 
 function getCustomcommandsHTML(guild) {

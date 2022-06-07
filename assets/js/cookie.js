7b295f0184fd298a0c8e33a6baff077731d37680
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-06-07 10:41:51 +0200

    krisääää

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -22,8 +22,8 @@ function getCookie(name) {
 }
 
 function deleteCookie(name) {
-	document.cookie = name + '=; Max-Age=-99999999;';
-	document.cookie = name + '=; Max-Age=-99999999; domain=.tomatenkuchen.eu;';
+	document.cookie = name + '=;Max-Age=-99999999;path=/;';
+	document.cookie = name + '=;Max-Age=-99999999;path=/;domain=.tomatenkuchen.eu;';
 }
 
 function fadeOut(element) {

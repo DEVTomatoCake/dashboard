eeae03fd85ad90ec353723d2cbfd680f2b109515
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-06-28 20:56:21 +0200

    fix cat toggle

--- assets/js/api/transformer.js
+++ assets/js/api/transformer.js
@@ -22,7 +22,7 @@ function getCommandsHTML() {
 					categories.forEach(category => {
 						text += '<table cellpadding="8" cellspacing="0" class="category" id="' + category + '">' +
 						'<caption>' + category.charAt(0).toUpperCase() + category.slice(1) + '</caption>' +
-						'<button id="' + category + 'tb" onclick="toggleCategory(\"' + category + '\");">Verstecken</button>' +
+						'<button id="' + category + 'tb" onclick="toggleCategory(\'' + category + '\');">Verstecken</button>' +
 						'<thead><tr><th>Name</th><th>Beschreibung</th><th>Verwendung</th></tr></thead><tbody>';
 						categoryData.forEach(data => {
 							if (category == data[0]) text += data[1];

020745c574d73531b9a700e3e5c2fb057f5adff2
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-07-21 17:15:22 +0200

    Better title + buttons on commands page

--- assets/js/api/transformer.js
+++ assets/js/api/transformer.js
@@ -20,14 +20,14 @@ function getCommandsHTML() {
 					});
 
 					categories.forEach(category => {
-						text += '<table cellpadding="8" cellspacing="0" class="category" id="' + category + '">' +
-						'<caption>' + category.charAt(0).toUpperCase() + category.slice(1) + '</caption>' +
-						'<button id="' + category + 'tb" onclick="toggleCategory(\'' + category + '\');">Verstecken</button>' +
+						text += '<center><h3>' + category.charAt(0).toUpperCase() + category.slice(1) + '</h3>' +
+						'<button class="categorybutton" id="' + category + 'tb" onclick="toggleCategory(\'' + category + '\');">Verstecken</button>' +
+						'<table cellpadding="8" cellspacing="0" class="category" id="' + category + '">' +
 						'<thead><tr><th>Name</th><th>Beschreibung</th><th>Verwendung</th></tr></thead><tbody>';
 						categoryData.forEach(data => {
 							if (category == data[0]) text += data[1];
 						});
-						text += '</tbody></table>';
+						text += '</tbody></table></center><br>';
 					});
 
 					resolve(text);

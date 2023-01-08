cbec9d8bcf89d3accdfbfd82a0aff868d265b3c3
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-01-08 15:24:31 +0100

    darkmode input style

--- assets/js/api/api.js
+++ assets/js/api/api.js
@@ -1,12 +1,11 @@
-const url = "https://api.tomatenkuchen.eu/api/";
-
+const url = "https://api.tomatenkuchen.eu/api/"
 async function get(component, auth) {
-	const response = await fetch(url + component + ((auth && getCookie("token")) ? (component.includes("?") ? "&" : "?") + "token=" + getCookie("token") : ""));
+	const response = await fetch(url + component + ((auth && getCookie("token")) ? (component.includes("?") ? "&" : "?") + "token=" + getCookie("token") : ""))
 
 	if (response.status != 429) {
-		const json = await response.json();
-		console.log("Response for \"" + url + component + "\": " + JSON.stringify(json));
-		return json;
+		const json = await response.json()
+		console.log("Response for \"" + url + component + "\": " + JSON.stringify(json))
+		return json
 	}
 }
 
@@ -14,36 +13,36 @@ function getCommands() {
 	return new Promise((resolve, reject) => {
 		get("commands/?lang=" + getLanguage(), false)
 			.then(data => resolve(data))
-			.catch(error => reject(error));
-	});
+			.catch(error => reject(error))
+	})
 }
 
 function getBotstats() {
 	return new Promise((resolve, reject) => {
 		get("stats", false)
 			.then(data => resolve(data))
-			.catch(error => reject(error));
-	});
+			.catch(error => reject(error))
+	})
 }
 
 function getGuilds() {
 	return new Promise((resolve, reject) => {
 		get("guilds", true)
 			.then(data => resolve(data))
-			.catch(error => reject(error));
-	});
+			.catch(error => reject(error))
+	})
 }
 
 function getStats(guild) {
 	return new Promise((resolve, reject) => {
 		get("stats/" + guild, true)
 			.then(data => resolve(data))
-			.catch(error => reject(error));
-	});
+			.catch(error => reject(error))
+	})
 }
 
 function login(code) {
-	const params = new URLSearchParams(window.location.search);
+	const params = new URLSearchParams(window.location.search)
 	return new Promise((resolve, reject) => {
 		get("auth/login?code=" + encodeURIComponent(code) + (params.get("state") ? "&dcState=" + params.get("state") : "") + (getCookie("clientState") ? "&state=" + getCookie("clientState") : "") + (location.hostname.startsWith("beta.") ? "&beta=true" : ""), false)
 			.then(data => {
@@ -53,38 +52,38 @@ function login(code) {
 			.catch(error => {
 				deleteCookie("clientState")
 				reject(error)
-			});
-	});
+			})
+	})
 }
 
 function logout() {
 	return new Promise((resolve, reject) => {
 		get("auth/logout", true)
 			.then(data => resolve(data))
-			.catch(error => reject(error));
-	});
+			.catch(error => reject(error))
+	})
 }
 
 function getLeaderboard(guild) {
 	return new Promise((resolve, reject) => {
 		get("leaderboard/" + guild, true)
 			.then(data => resolve(data))
-			.catch(error => reject(error));
-	});
+			.catch(error => reject(error))
+	})
 }
 
 function getDataexport() {
 	return new Promise((resolve, reject) => {
 		get("users/dataexport", true)
 			.then(data => resolve(data))
-			.catch(error => reject(error));
-	});
+			.catch(error => reject(error))
+	})
 }
 
 function getTickets(guild) {
 	return new Promise((resolve, reject) => {
 		get("tickets/" + guild, true)
 			.then(data => resolve(data))
-			.catch(error => reject(error));
-	});
+			.catch(error => reject(error))
+	})
 }

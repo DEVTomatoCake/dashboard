4baa06242eef8b25722a92c625d0ce852a5d0f1b
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-01-08 13:03:21 +0100

    finish service worker + add shortcuts

--- serviceworker.js
+++ serviceworker.js
@@ -1,36 +1,33 @@
 self.addEventListener("install", event => {
-	console.log("Service worker installed")
-
 	event.waitUntil((async () => {
 		const cache = await caches.open("offline")
-		await cache.addAll(["/offline"])
+		await cache.addAll(["/offline", "/", "/assets/style.css", "/assets/js/script.js"])
 	})())
 })
 self.addEventListener("activate", event => {
-	console.log("Service worker activated")
-
 	event.waitUntil((async () => {
 		if ("navigationPreload" in self.registration) await self.registration.navigationPreload.enable()
 	})())
 
 	self.clients.claim()
 })
+
 self.addEventListener("fetch", event => {
 	if (event.request.method == "GET" && event.request.mode == "navigate") {
-		console.log(event.request)
-		console.log("Handling fetch event for", event.request.url)
-		event.respondWith((async () => {
+	  	event.respondWith((async () => {
 			try {
 				const preloadResponse = await event.preloadResponse
        			if (preloadResponse) return preloadResponse
 
 				const response = await fetch(event.request)
+				cache.put(event.request, response.clone())
 				return response
 			} catch (e) {
-				console.warn("Cannot fetch, serving offline page", e)
+				console.warn("Cannot fetch, serving from cache", e)
 
 				const cache = await caches.open("offline")
-				const cachedResponse = await cache.match("/offline")
+				var cachedResponse = await cache.match(event.request.url)
+				if (!cachedResponse) return cachedResponse = await caches.match("/offline")
 				return cachedResponse
 			}
 		})())

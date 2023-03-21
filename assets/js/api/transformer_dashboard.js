594d70e3bccccfa9014c97c89bea012ee4f5e445
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-03-21 13:50:52 +0100

    testing

--- assets/js/api/transformer_dashboard.js
+++ assets/js/api/transformer_dashboard.js
@@ -54,7 +54,7 @@ function getSettingsHTML(json) {
 
 					if (setting.value.length > 0 && typeof setting.value[0] == "object") temp += Object.keys(setting.value).map(i => addItem(setting, possible, i, setting.value[i], void 0, true)).join("");
 					else if (setting.value.length > 0) temp += setting.value.map(i => addItem(setting, possible, i)).join("");
-					else if (Object.keys(setting.value).length > 0) temp += Object.keys(setting.type).map(i => addItem(setting, possible, i, setting.value[i])).join("");
+					else if (Object.keys(setting.value).length > 0) temp += addItem(setting, possible, setting.value)//Object.keys(setting.type).map(i => addItem(setting, possible, i, setting.value[i])).join("");
 					temp += "</div>";
 				} else {
 					temp += "<select class='setting' id='" + setting.key + "'>";

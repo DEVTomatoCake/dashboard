28247fc47ea9d5fc4fe84b856985d465d6a1bf7b
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-04-23 09:26:14 +0200

    .-. log del fix

--- assets/js/api/transformer_dashboard.js
+++ assets/js/api/transformer_dashboard.js
@@ -365,7 +365,7 @@ function getLogsHTML(guild) {
 							"<td>" +
 								"<button type='button' class='categorybutton' onclick='info(\"" + encode(log.id) + "\")' translation='logs.moreinfo'></button>" +
 								(log.date < Date.now() - 1000 * 60 * 60 * 24 * 5 ? "<button type='button' class='categorybutton red' onclick='const c=confirm(\"Do you really want to delete the log \\\"" +
-								encode(log.id) + "\\\"?\");if(c){deleteLog(\"" + encode(guild) + "\",\"" + encode(log.id) + "\");this.parentElement.remove();}' translation='logs.delete'></button>" : "") +
+								encode(log.id) + "\\\"?\");if(c){deleteLog(\"" + encode(guild) + "\",\"" + encode(log.id) + "\");this.parentElement.parentElement.remove();}' translation='logs.delete'></button>" : "") +
 							"</td>" +
 							"</tr>";
 					});

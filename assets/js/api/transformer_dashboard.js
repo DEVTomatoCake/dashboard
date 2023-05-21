23a6fed53a8851721928337ac7d692fdd5843c20
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-05-21 10:22:53 +0200

    loading fix

--- assets/js/api/transformer_dashboard.js
+++ assets/js/api/transformer_dashboard.js
@@ -160,9 +160,10 @@ function getActionsHTML(json) {
 				"<label for='" + encode(action.name) + "'><b>" + encode(action.name) + "</b></label>" +
 				"<div class='emoji-container'>" +
 				(action.source ? "<p>Source: " + encode(action.source) + "</p>" : "") +
-				(action.content ? "<textarea class='code' rows='" + (Math.round(action.content.split("\n").length * 1.2) + 2) + "' id='" + encode(action.name) + "-action' maxlength='2000' name='" + encode(action.name) : "") +
-				(action.slashcommand ? "' data-slashcommand='" + encode(action.slashcommand) : "") + "' data-trigger='" + encode(action.trigger) + "' data-version='" + encode(action.version) + "'>" +
-				action.content + "</textarea><ion-icon name='at-outline' title='Rolepicker' onclick='mentionPicker(this.parentElement, pickerData.roles)'></ion-icon>" +
+				"<textarea class='code' rows='" + (action.content ? Math.round(action.content.split("\n").length * 1.2) + 2 : 1) + "' id='" + encode(action.name) + "-action' maxlength='2000' name='" + encode(action.name) +
+				(action.slashcommand ? "' data-slashcommand='" + encode(action.slashcommand) : "") + "' data-trigger='" + encode(action.trigger) + "' data-version='" + encode(action.version) + "'" +
+				(action.content ? "" : " readonly") + ">" + (action.content || "") +
+				"</textarea><ion-icon name='at-outline' title='Rolepicker' onclick='mentionPicker(this.parentElement, pickerData.roles)'></ion-icon>" +
 				"<ion-icon name='happy-outline' title='Emojipicker' onclick='emojiPicker(this.parentElement, pickerData.emojis, guildName)'></ion-icon>" +
 				"</div>" +
 				"<ion-icon name='trash-outline' onclick='deleteAction(this, \"" + encode(action.name) + "\");'></ion-icon>" +

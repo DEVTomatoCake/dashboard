b7689710561bbf9412d59fabe85f40003e1b4b60
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-06-24 07:48:28 +0200

    add docs link

--- assets/js/api/api.js
+++ assets/js/api/api.js
@@ -19,7 +19,7 @@ const getCommands = () => new Promise((resolve, reject) => {
 })
 
 const getBotstats = () => new Promise((resolve, reject) => {
-	get("stats?lang=" + getLanguage(), false)
+	get("stats?lang=" + getLanguage() + "&publicServers=1", false)
 		.then(d => resolve(d)).catch(e => reject(e))
 })
 

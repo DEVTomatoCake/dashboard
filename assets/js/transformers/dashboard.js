4e58e6f60ab4b4fea485dfece26dd68d14c80bc3
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-07-04 09:24:44 +0200

    page select fix

--- assets/js/transformers/dashboard.js
+++ assets/js/transformers/dashboard.js
@@ -12,9 +12,9 @@ function getGuildsHTML() {
 					}).forEach(guild => {
 						text +=
 							"<div class='guilds-container'>" +
-							"<a class='guild' href='" + (guild.active ? "./settings" : "/invite") + "?guild=" + guild.id +
+							"<a class='guild' href='/" + (guild.active ? "dashboard/settings" : "invite") + "?guild=" + guild.id +
 							(target && target.split("?")[1] ? "&" + target.split("?")[1].replace(/[^\w=-]/gi, "") : "") + "'>" +
-							"<img" + (guild.active ? "" : " class='inactive'") + " alt='" + guild.id + "' width='128' height='128' title='" + encode(guild.name) + "' src='" + guild.icon + "'>" +
+							"<img" + (guild.active ? "" : " class='inactive'") + " alt='" + encode(guild.name) + " Server icon' width='128' height='128' title='" + encode(guild.name) + "' src='" + encode(guild.icon) + "'>" +
 							"<div class='text'>" + encode(guild.name) + "</div>" +
 							"</a>" +
 							"</div>";

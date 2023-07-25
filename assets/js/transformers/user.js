821a9217fc947542868a2e25b08505a42e307dde
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-07-25 16:35:43 +0200

    improve design

--- assets/js/transformers/user.js
+++ assets/js/transformers/user.js
@@ -14,19 +14,19 @@ loadFunc = async () => {
 					(site.status == 3 ? "<small>This site doesn't allow me to display whether you voted or not, however you still receive your credits!</small>" : "") +
 					(site.next ? "<br><ion-icon name='arrow-forward-outline'></ion-icon> Next vote in <b>" + encode(site.next) + "</b>" : "")
 			})
-			document.getElementById("vote-tk").innerHTML = text
+			document.getElementById("vote-tk").innerHTML = text + "</ul>"
 
-			let textmb = ""
+			let textmb = "<ul>"
 			json.data.filter(site => site.status != 0 && site.mb).forEach(site => {
 				textmb +=
 					"<li>" +
 					"<a href='" + encode(site.url) + "'>" + encode(site.name) + "</a>" +
-					(site.status == 1 ? "You can vote now!" : "") +
-					(site.status == 2 ? "You've already voted - thank you!" : "") +
-					(site.status == 3 ? "<small>This site doesn't allow me to display whether you voted or not, however you still receive your credits!</small>" : "") +
+					(site.status == 1 ? "<br><ion-icon name='thumbs-up-outline'></ion-icon>You can vote now!" : "") +
+					(site.status == 2 ? "<br><ion-icon name='heart'></ion-icon>You've already voted - thank you!" : "") +
+					(site.status == 3 ? "<br><ion-icon name='warning-outline'></ion-icon><small>This site doesn't allow me to display whether you've voted or not, however you still receive your credits!</small>" : "") +
 					(site.next ? "<br><ion-icon name='arrow-forward-outline'></ion-icon> Next vote in <b>" + encode(site.next) + "</b>" : "")
 			})
-			document.getElementById("vote-mb").innerHTML = textmb
+			document.getElementById("vote-mb").innerHTML = textmb + "</ul>"
 		} else return handleError(resolve, json.message)
 
 		document.getElementById("linksidebar").innerHTML +=

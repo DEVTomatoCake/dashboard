d21f6a1fa23b0bf7f9c3efd7ae69af08d74946c6
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-07-28 13:51:36 +0200

    user vote links fix

--- assets/js/transformers/user.js
+++ assets/js/transformers/user.js
@@ -1,32 +1,19 @@
+const formatVote = site => {
+	return "<li>" +
+		"<a href='" + encode(site.url) + "'>" + encode(site.name) + "</a>" +
+		(site.status == 1 ? "<br><ion-icon name='thumbs-up-outline'></ion-icon>You can vote now!" : "") +
+		(site.status == 2 ? "<br><ion-icon name='heart'></ion-icon>You've already voted - thank you!" : "") +
+		(site.status == 3 ? "<br><ion-icon name='warning-outline'></ion-icon><small>This site doesn't allow me to display whether you've voted or not, however you still receive your credits!</small>" : "") +
+		(site.next ? "<br><ion-icon name='arrow-forward-outline'></ion-icon> Next vote in <b>" + encode(site.next) + "</b>" : "")
+}
+
 loadFunc = async () => {
 	if (getCookie("token")) {
 		const json = await getUser()
 
 		if (json.status == "success") {
-			let text = "<ul>"
-
-			json.data.filter(site => site.status != 0 && !site.mb).forEach(site => {
-				text +=
-					"<li>" +
-					"<a href='" + encode(site.url) + "'>" + encode(site.name) + "</a>" +
-					(site.status == 1 ? "You can vote now!" : "") +
-					(site.status == 2 ? "You've already voted - thank you!" : "") +
-					(site.status == 3 ? "<small>This site doesn't allow me to display whether you voted or not, however you still receive your credits!</small>" : "") +
-					(site.next ? "<br><ion-icon name='arrow-forward-outline'></ion-icon> Next vote in <b>" + encode(site.next) + "</b>" : "")
-			})
-			document.getElementById("vote-tk").innerHTML = text + "</ul>"
-
-			let textmb = "<ul>"
-			json.data.filter(site => site.status != 0 && site.mb).forEach(site => {
-				textmb +=
-					"<li>" +
-					"<a href='" + encode(site.url) + "'>" + encode(site.name) + "</a>" +
-					(site.status == 1 ? "<br><ion-icon name='thumbs-up-outline'></ion-icon>You can vote now!" : "") +
-					(site.status == 2 ? "<br><ion-icon name='heart'></ion-icon>You've already voted - thank you!" : "") +
-					(site.status == 3 ? "<br><ion-icon name='warning-outline'></ion-icon><small>This site doesn't allow me to display whether you've voted or not, however you still receive your credits!</small>" : "") +
-					(site.next ? "<br><ion-icon name='arrow-forward-outline'></ion-icon> Next vote in <b>" + encode(site.next) + "</b>" : "")
-			})
-			document.getElementById("vote-mb").innerHTML = textmb + "</ul>"
+			document.getElementById("vote-tk").innerHTML = "<ul>" + json.data.filter(site => site.status != 0 && !site.mb).map(formatVote) + "</ul>"
+			document.getElementById("vote-mb").innerHTML = "<ul>" + json.data.filter(site => site.status != 0 && site.mb).map(formatVote) + "</ul>"
 		} else return handleError(resolve, json.message)
 
 		document.getElementById("linksidebar").innerHTML +=

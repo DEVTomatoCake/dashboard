86b52e520deabbc7e0e61c24298cfe3ffacf6bb1
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-08-12 13:22:14 +0200

    display more info on user page

--- assets/js/transformers/user.js
+++ assets/js/transformers/user.js
@@ -1,9 +1,10 @@
 const formatVote = site => {
-	return "<a href='" + encode(site.url) + "' target='_blank' rel='noopener'>" + encode(site.name) + "</a>" +
+	return "<li><a href='" + encode(site.url) + "' target='_blank' rel='noopener'>" + encode(site.name) + "</a>" +
 		(site.status == 1 ? "<br><ion-icon name='thumbs-up-outline'></ion-icon>You can vote now!" : "") +
 		(site.status == 2 ? "<br><ion-icon name='heart'></ion-icon>You've already voted - thank you!" : "") +
 		(site.status == 3 ? "<br><small><ion-icon name='warning-outline'></ion-icon>This site doesn't allow me to display whether you've voted or not, however you still receive your credits!</small>" : "") +
-		(site.next ? "<br> <ion-icon name='arrow-forward-outline'></ion-icon>Next vote in <b>" + encode(site.next) + "</b>" : "")
+		(site.next ? "<br> <ion-icon name='arrow-forward-outline'></ion-icon>Next vote in <b>" + encode(site.next) + "</b>" : "") +
+		"</li>"
 }
 
 loadFunc = async () => {
@@ -11,8 +12,10 @@ loadFunc = async () => {
 		const json = await getUser()
 
 		if (json.status == "success") {
-			document.getElementById("vote-tk").innerHTML = "<ul><li>" + json.data.filter(site => site.status != 0 && !site.mb).map(formatVote).join("</li><li>") + "</li></ul>"
-			document.getElementById("vote-mb").innerHTML = "<ul><li>" + json.data.filter(site => site.status != 0 && site.mb).map(formatVote).join("</li><li>") + "</li></ul>"
+			document.getElementById("vote-tk").innerHTML = "<p>In total, you've voted <b>" + json.totalVotes + "</b> times for TomatenKuchen.<br>" +
+				"Also, you currently have <b>" + json.credits + "</b> credits.</p>" +
+				"<ul>" + json.votes.filter(site => site.status != 0 && !site.mb).map(formatVote).join("") + "</ul>"
+			document.getElementById("vote-mb").innerHTML = "<ul>" + json.votes.filter(site => site.status != 0 && site.mb).map(formatVote).join("") + "</ul>"
 		} else return handleError(resolve, json.message)
 
 		document.getElementById("linksidebar").innerHTML =

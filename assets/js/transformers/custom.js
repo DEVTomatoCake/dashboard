5725b79fd2ec5ece7f395bfd83e28d2b6af9f2a2
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-10-01 14:57:25 +0200

    update custom price formula

--- assets/js/transformers/custom.js
+++ assets/js/transformers/custom.js
@@ -1,34 +1,20 @@
-let tiers = []
 const calcCredits = () => {
 	const elem = document.getElementById("calc-credits")
-	const value = Math.abs(parseInt(elem.value || elem.getAttribute("placeholder")))
-	if (value < 2 || value > 5000000) return
+	const userCount = Math.abs(parseInt(elem.value || elem.getAttribute("placeholder")))
+	if (userCount < 2 || userCount > 5000000) return
 
-	let cost = 0
-	for (const tier of tiers) {
-		if (value >= tier.min) cost = tier.cost + value * tier.percent
-	}
-
-	document.getElementById("calc-result").innerHTML = "<b>" + cost.toLocaleString() + "</b> credits per day - that's just voting <b>" +
+	const cost = (Math.pow(50 * (userCount + 100), 0.6) - 115).toFixed(1)
+	document.getElementById("calc-result").innerHTML = "<b>" + parseFloat(cost).toLocaleString() + "</b> credits per day - that's just voting <b>" +
 		Math.ceil(cost / 28).toLocaleString() + "</b> times every 24 hours <small>(and less if members vote more often)</small>!"
 }
 
 loadFunc = async () => {
+	calcCredits()
+
 	const json = await getCustomTiers()
 	if (json.status == "success") {
-		tiers = json.tiers
-		calcCredits()
-
-		document.getElementById("tiers-cost").innerHTML = tiers.map((tier, i) =>
-			"<tr><td>" + tier.min.toLocaleString() + (tiers[i + 1] ? " - " + (tiers[i + 1].min - 1).toLocaleString() : "+") +
-			"</td><td>" + tier.cost.toLocaleString() + "</td><td>" + (tier.percent * 100).toLocaleString() + "% × user count</td></tr>"
-		).join("")
-
 		document.getElementById("tiers-upgrade").innerHTML = json.upgrades.map(tier =>
 			"<tr><td>" + tier.cost.toLocaleString() + "</td><td>" + encode(tier.text) + "</td></tr>"
 		).join("")
-	} else {
-		document.getElementById("tiers-cost").innerHTML = "<tr><td colspan='3'>Failed to load tiers</td></tr>"
-		document.getElementById("tiers-upgrade").innerHTML = "<tr><td colspan='2'>Failed to load tiers</td></tr>"
-	}
+	} else document.getElementById("tiers-upgrade").innerHTML = "<tr><td colspan='2'>Failed to load upgrades</td></tr>"
 }

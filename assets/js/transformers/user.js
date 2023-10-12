a3cb4220071970c9702dc391239a781f0f5a6bb8
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-10-12 19:08:04 +0200

    Fancier vote site display

--- assets/js/transformers/user.js
+++ assets/js/transformers/user.js
@@ -1,19 +1,21 @@
 const formatVote = site => {
 	return "<li><a href='" + encode(site.url) + "' target='_blank' rel='noopener'>" + encode(site.name) + "</a>" +
-		(site.status == 1 ? "<br><ion-icon name='thumbs-up-outline'></ion-icon><span translation='user.canvote'></span>" : "") +
-		(site.status == 2 ? "<br><ion-icon name='heart'></ion-icon><span translation='user.voted'></span>" : "") +
-		(site.status == 3 ? "<br><small><ion-icon name='warning-outline'></ion-icon><span translation='user.voteunknown'></span></small>" : "") +
-		(site.next ? "<br> <ion-icon name='arrow-forward-outline'></ion-icon>Next vote in <b>" + encode(site.next) + "</b>" : "") +
+		(site.status == 1 ? "<br><span class='vmiddle'><ion-icon name='thumbs-up-outline'></ion-icon><span translation='user.canvote'></span></span>" : "") +
+		(site.status == 2 ? "<br><span class='vmiddle'><ion-icon name='heart'></ion-icon><span translation='user.voted'></span></span>" : "") +
+		(site.status == 3 ? "<br><small class='vmiddle'><ion-icon name='warning-outline'></ion-icon><span translation='user.voteunknown'></span></small>" : "") +
+		(site.next ? "<br><span class='vmiddle'><ion-icon name='arrow-forward-outline'></ion-icon>Next vote in <b>" + encode(site.next) + "</b></span>" : "") +
 		"</li>"
 }
 
+let sites = []
 loadFunc = async () => {
 	if (getCookie("token")) {
 		const json = await getUser()
+		sites = json.votes
 
 		if (json.status == "success") {
 			document.getElementById("vote-tk").innerHTML = "<p>In total, you've voted <b>" + json.totalVotes.toLocaleString() + "</b> times for TomatenKuchen.<br>" +
-				"Also, you currently have <b>" + json.credits.toLocaleString() + " credits</b>.</p><br><h3 translation='user.votelinks'></h3>" +
+				"You currently have <b>" + json.credits.toLocaleString() + " credits</b>.</p><br><h3 translation='user.votelinks'></h3>" +
 				"<ul>" + json.votes.filter(site => site.status != 0 && !site.mb).map(formatVote).join("") + "</ul>"
 			document.getElementById("vote-mb").innerHTML = "<ul>" + json.votes.filter(site => site.status != 0 && site.mb).map(formatVote).join("") + "</ul>"
 		} else return handleError(resolve, json.message)

a0ba3c0b61dea9bf8c6113fc40c9cbe579d9e9ac
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-11-12 07:04:04 +0100

    fun with service workers

--- serviceworker.js
+++ serviceworker.js
@@ -5,15 +5,24 @@ self.addEventListener("install", event => {
 			"/offline",
 			"/assets/style.css",
 			"/assets/js/script.js",
+			"/assets/js/language.js",
 			"/",
-
+			"/commands",
 			"/dashboard",
-			"/dashboard/settings",
-			"/assets/emojipicker.css",
-			"/assets/toasts.css"
+			"/manifest.json",
+			"/assets/images/favicon.ico",
+			"/assets/images/buttonroles.webp",
+			"/assets/images/tomato_ban.webp",
+			"/assets/images/tomato_empty.webp",
+			"/assets/images/wikimedia_flagde.svg",
+			"/assets/images/wikimedia_flagen.svg",
+			"/assets/images/wikimedia_flagfr.svg",
+			"/assets/images/wikimedia_flaghu.svg",
+			"/assets/images/wikimedia_flagja.svg"
 		])
 	})())
 })
+
 self.addEventListener("activate", event => {
 	event.waitUntil((async () => {
 		if ("navigationPreload" in self.registration) await self.registration.navigationPreload.enable()
@@ -25,18 +34,19 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
 	if (event.request.method == "GET" && event.request.mode == "navigate") {
 		event.respondWith((async () => {
+			console.log(event.request.url)
 			try {
 				const preloadResponse = await event.preloadResponse
 				if (preloadResponse) return preloadResponse
 
 				const response = await fetch(event.request)
-				cache.put(event.request, response.clone())
+				cache.add(event.request, response.clone())
 				return response
 			} catch (e) {
 				console.warn("Cannot fetch " + event.request.url + ", serving from cache", e)
 
 				const cache = await caches.open("offline")
-				const cachedResponse = await cache.match(event.request.url)
+				const cachedResponse = await cache.match(event.request)
 				if (!cachedResponse) return await caches.match("/offline")
 				return cachedResponse
 			}

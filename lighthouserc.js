84d1626c7015f2156013def5371ee3f8171c536d
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-12-20 21:37:00 +0100

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,8 +5,15 @@ module.exports = {
 		},
 		collect: {
 			url: ["http://localhost:4269"],
-			startServerCommand: "node dev.js"
-			//staticDistDir: "./"
+			startServerCommand: "node dev.js",
+			startServerReadyPattern: "^Running on .+",
+			startServerReadyTimeout: 15000/*
+			staticDistDir: "./",
+			url: [
+				"./index.html",
+				"./commands.html",
+				"./privacy.html"
+			]*/
 		},
 		upload: {
 			// Longterm: https://github.com/GoogleChrome/lighthouse-ci/blob/main/docs/getting-started.md#the-lighthouse-ci-server

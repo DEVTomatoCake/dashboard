97b6d6fec5cfa93fad15ab9cee4d61f510222dc9
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2024-01-20 21:03:18 +0100

    msg editor load with current value

--- assets/js/messageeditor.js
+++ assets/js/messageeditor.js
@@ -1,7 +1,28 @@
 const toggleMsgEditor = settingsKey => {
-	openDialog(document.getElementById("msgeditor-dialog"))
+	const dialog = document.getElementById("msgeditor-dialog")
+	dialog.removeAttribute("hidden")
+	dialog.getElementsByClassName("close")[0].onclick = () => {
+		dialog.setAttribute("hidden", "")
+		document.getElementById("msgeditor-iframe").src = "about:blank"
+	}
 
-	const setting = settingsData[settingsKey]
-	console.log(setting)
-	document.getElementById("msgeditor-iframe").src = "https://embed.tomatenkuchen.com/?data=" + btoa(JSON.stringify({content: "yay!"}))
+	const msg = settingsData.find(set => set.key == settingsKey).value[0]
+	document.getElementById("msgeditor-iframe").src = "https://embed.tomatenkuchen.com/?data=" + btoa(encodeURIComponent(JSON.stringify({
+		content: msg.content || void 0,
+		embeds: [{
+			color: msg.color || void 0,
+			title: msg.title || void 0,
+			description: msg.description || void 0,
+			image: msg.image || void 0,
+			thumbnail: msg.thumbnail || void 0,
+			author: {
+				name: msg.author || void 0,
+				icon_url: msg.authoricon || void 0
+			},
+			footer: {
+				text: msg.footer || void 0,
+				icon_url: msg.footericon || void 0
+			}
+		}]
+	})))
 }

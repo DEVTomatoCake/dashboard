e7d7acd7393de159fe854b510fae00d2118eab35
booky10 <boooky10@gmail.com>
2024-09-08 23:15:11 +0200

    Fix code style issues with eslint

--- assets/js/legal_contact.js
+++ assets/js/legal_contact.js
@@ -1,26 +1,26 @@
 /** @type {HTMLButtonElement} */
-const button = document.getElementById("request-button");
+const button = document.getElementById("request-button")
 
 const triggerContact = async () => {
-  const email = document.getElementById("email-input").value;
-  const name = document.getElementById("name-input").value;
-  const request = document.getElementById("request-input").value;
+  const email = document.getElementById("email-input").value
+  const name = document.getElementById("name-input").value
+  const request = document.getElementById("request-input").value
 
-  const infoElement = document.getElementById("input-info");
+  const infoElement = document.getElementById("input-info")
   if (!email || !name || !request) {
-    infoElement.textContent = "All form fields need to be filled!";
-    return;
+    infoElement.textContent = "All form fields need to be filled!"
+    return
   }
 
-  infoElement.textContent = "Submitting contact form...";
-  button.disabled = true;
+  infoElement.textContent = "Submitting contact form..."
+  button.disabled = true
 
   await fetch("https://api.tomatenkuchen.com/contact", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, name, request }),
-  });
+    body: JSON.stringify({ email, name, request })
+  })
 
-  infoElement.textContent = "The contact form has been submitted!";
-  button.disabled = false;
-};
+  infoElement.textContent = "The contact form has been submitted!"
+  button.disabled = false
+}

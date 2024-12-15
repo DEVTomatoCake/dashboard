d5cf9f91136632c449ebe87c9e28610361454740
booky10 <boooky10@gmail.com>
2024-12-15 23:52:24 +0100

    Temporarily update embedbuilder links

--- assets/js/messageeditor.js
+++ assets/js/messageeditor.js
@@ -5,9 +5,9 @@ const toggleMsgEditor = (setting = "", msgId = "") => {
 	document.getElementById("msgeditor-save").onclick = () => {
 		dialog.setAttribute("hidden", "")
 
-		document.getElementById("msgeditor-iframe").contentWindow.postMessage("requestMessage", "https://embed.tomatenkuchen.com")
+		document.getElementById("msgeditor-iframe").contentWindow.postMessage("requestMessage", "https://tk-embed.chaoshosting.eu")
 		window.onmessage = e => {
-			if (e.origin == "https://embed.tomatenkuchen.com" && e.data.startsWith("respondMessage_")) {
+			if (e.origin == "https://tk-embed.chaoshosting.eu" && e.data.startsWith("respondMessage_")) {
 				messageData[document.getElementById("msgeditor-iframe").dataset.current] = JSON.parse(decodeURIComponent(atob(e.data.replace("respondMessage_", ""))))
 				document.getElementById("msgeditor-iframe").src = "about:blank"
 				handleChange(setting)
@@ -18,7 +18,7 @@ const toggleMsgEditor = (setting = "", msgId = "") => {
 		dialog.setAttribute("hidden", "")
 	}
 
-	document.getElementById("msgeditor-iframe").src = "https://embed.tomatenkuchen.com/?data=" + btoa(encodeURIComponent(JSON.stringify(messageData[msgId]))) +
+	document.getElementById("msgeditor-iframe").src = "https://tk-embed.chaoshosting.eu/?data=" + btoa(encodeURIComponent(JSON.stringify(messageData[msgId]))) +
 		(getCookie("theme") == "light" ? "&light=1" : "")
 	document.getElementById("msgeditor-iframe").dataset.current = msgId
 }
